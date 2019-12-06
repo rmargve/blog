71dc334ab720ae45d9aa425ca90d54037dec2335
Midnight <rmargve@gmail.com>
2019-12-06 20:24:34 +0400

    update logo margin

--- src/components/nav.js
+++ src/components/nav.js
@@ -4,7 +4,7 @@ import React from "react"
 import "../styles/style.css"
 
 export default () => (
-  <nav className="text-teal-500 inline-block font-bold text-3xl mb-4 lg:m-16">
+  <nav className="text-teal-500 inline-block font-bold text-3xl mb-8 lg:m-16">
     <Link to="/">Midnight</Link>
   </nav>
 )
