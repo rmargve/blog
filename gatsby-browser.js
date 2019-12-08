f1c018e38110ddbdced60d2471a030b7e7cb2070
Midnight <rmargve@gmail.com>
2019-12-09 00:46:16 +0400

    add gatsby-browser with fontawesome css preload

---
+++ gatsby-browser.js
@@ -0,0 +1,5 @@
+import "@fortawesome/fontawesome-svg-core/styles.css"
+import { config } from "@fortawesome/fontawesome-svg-core"
+
+// You should do that in a Layout file or in `gatsby-browser.js`.
+config.autoAddCss = false
